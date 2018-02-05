69bbc810a936dc8ca2d582993e1f7448e5e94ff0
RuiwenP2018 <rpan@sjnma.org>
2018-02-05 10:21:01 -0600

    python fille update

---
+++ variables.py
@@ -0,0 +1,17 @@
+print("hello world")
+print("hello world")
+print("hello world")
+
+
+print('''hello world
+this is my line
+''')
+
+
+first_name = raw_input("what is your first name?")
+
+last_name = raw_input("what is your last name?")
+
+full_name = first_name +" "+ last_name
+
+print("Yes, that works. "+full_name)
