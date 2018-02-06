0e677db3f09266bfc8e6a754993f2c52eb618040
RuiwenP2018 <rpan@sjnma.org>
2018-02-06 10:08:01 -0600

    update python class work

--- variables.py
+++ variables.py
@@ -13,5 +13,16 @@ first_name = raw_input("what is your first name?")
 last_name = raw_input("what is your last name?")
 
 full_name = first_name +" "+ last_name
-
+#string^
 print("Yes, that works. "+full_name)
+
+number1 = 500
+#integer^
+number2 = 1000.0
+#floating points^
+number3 = number1 / number2
+
+print(number3)
+
+MrPanIsGoodMood = True
+#boolean^
