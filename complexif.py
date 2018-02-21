9c157f081845af9cb530ceca2faa05af0a153e56
RuiwenP2018 <rpan@sjnma.org>
2018-02-21 14:32:30 -0600

    non

---
+++ complexif.py
@@ -0,0 +1,28 @@
+name = raw_input ("Enter your firt name. ")
+
+password = raw_input ("Enter your password. ")
+
+if ((name == "omer" and password == "dinosaur") or (name == "pan" and password == "matter")):
+
+	print ("you are logged in ")
+
+else:
+
+	print ("access denied.")
+
+if (name == "omer" or name == "tom" or name == "trent"):
+
+	print ("thats a cool name")
+
+else:
+
+	print ("hmm I don't know that name. ")
+
+if (not (name == "omer" or name == "tom" or name == "trent")):
+
+        print ("thats a bad name")
+
+else:
+
+        print ("hmm I don't know that name. ")
+
